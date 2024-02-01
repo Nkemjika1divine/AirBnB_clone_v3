45d15b7a51d9877d82c9644a9242197e5689594c
Nkemjika1divine <cndivine@gmail.com>
2024-02-01 11:08:16 -0800

    stats fix: retreiving data by count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,3 +7,24 @@ from api.v1.views import app_views
 def status():
     """get api status"""
     return {"status": "OK"}
+
+@app_views.route("/stats")
+def stats():
+    """gets the number of all classes"""
+    from models import storage
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+
+    count = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
+    }
+    return count
