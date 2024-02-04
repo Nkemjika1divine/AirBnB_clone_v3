892366409ecf6f8ac67eabe5f10ae7a36d57596b
Nkemjika1divine <cndivine@gmail.com>
2024-02-04 12:23:07 -0800

    404 error: not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 """Documentaton for module"""
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handles the error message '404: not found'"""
+    message = {
+            "error": "not found"
+        }
+    return jsonify(message)
 
 
 @app.teardown_appcontext
